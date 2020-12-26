61d77c937cbbd55832f7fe09c4022ee5b294b04a
Albert Ibars <a.ibars99@gmail.com>
2020-12-26 18:27:15 +0100

    avanzando

--- polygons.py
+++ polygons.py
@@ -40,7 +40,6 @@ class ConvexPolygon:
     # Get the number of vertices and edges of a convex polygon
     def getVerticesEdges(self):
         n = len(self.coordinates)
-        print(self.coordinates)
         # empty polygon
         if n == 0:
             return 0, 0
@@ -54,16 +53,47 @@ class ConvexPolygon:
             return n, n
 
     # Get the length of the perimeter of a convex polygon
-    def getPerimeter(self, polygon):
-        print(polygon)
+    def getPerimeter(self):
+        length = 0
+        for i in range(len(self.coordinates)):
+            c1 = self.coordinates[i]
+            c2 = self.coordinates[(i + 1) % len(self.coordinates)]
+            length += ConvexPolygon.distance(c1, c2)
+        return round(length, 3)
 
     # Get the area of a convex polygon
-    def getArea(self, polygon):
-        print(polygon)
+    def getArea(self):
+        psum, nsum = 0, 0
+        for i in range(len(self.coordinates)):
+            sindex = (i + 1) % len(self.coordinates)
+            prod = self.coordinates[i][0] * self.coordinates[sindex][1]
+            psum += prod
+
+        for i in range(len(self.coordinates)):
+            sindex = (i + 1) % len(self.coordinates)
+            prod = self.coordinates[sindex][0] * self.coordinates[i][1]
+            nsum += prod
+
+        return round(abs(psum - nsum) / 2, 3)
 
     # Get the coordinates of the centroid of a convex polygon
-    def getCoordCentroid(self, polygon):
-        print(polygon)
+    def getCoordCentroid(self):
+        centroid = [0, 0]
+        det = 0
+        for i in range(len(self.coordinates)):
+            c1 = self.coordinates[i]
+            c2 = self.coordinates[(i + 1) % len(self.coordinates)]
+
+            # compute the determinant
+            tempDet = c1[0] * c2[1] - c2[0] * c1[1]
+            det += tempDet
+
+            centroid[0] += (c1[0] + c2[0]) * tempDet
+            centroid[1] += (c1[1] + c2[1]) * tempDet
+
+        # divide by the total mass of the polygon
+        centroid = map(lambda x: round(x / float(3 * det), 3), centroid)
+        return centroid
 
     # Check if a convex polygon is regular
     def isRegular(self, polygon):
@@ -130,3 +160,9 @@ class ConvexPolygon:
         p1 = [c2[0] - origin[0], c2[1] - origin[1]]
         p2 = [c1[0] - origin[0], c1[1] - origin[1]]
         return p1[0] * p2[1] - p2[0] * p1[1]
+
+    @staticmethod
+    def distance(c1, c2):
+        xd = c1[0] - c2[0]
+        yd = c1[1] - c2[1]
+        return (xd ** 2 + yd ** 2) ** 0.5
--- test.py
+++ test.py
@@ -2,21 +2,16 @@ import polygons
 
 
 def main():
-    # cordenadas1 = [[1, 4], [5, 5], [5, 2], [3, 3], [3, 1], [9, 6], [7, 0], [0, 0]]
-    # cordenadas2 = [[3, 3], [5, 2], [3, 1]]
-    cordenadas1 = [[1, 4], [9, 6]]
-    cordenadas2 = [[13, 7]]
+    cordenadas1 = [[1, 4], [5, 5], [5, 2], [3, 3], [3, 1], [9, 6], [7, 0], [0, 0]]
+    cordenadas2 = [[3, 3], [5, 2], [3, 1]]
     polygon1 = polygons.ConvexPolygon(cordenadas1)
     polygon2 = polygons.ConvexPolygon(cordenadas2)
-    point = [5, 5]
-
-    if polygon1.containsPoint(point):
-        print(True)
-    else:
-        print(False)
 
     print(polygon1.getVerticesEdges())
 
+    print("Perimeter: " + str(polygon1.getPerimeter()))
+    print("Area: " + str(polygon1.getArea()))
+    print("Centroid: " + str(polygon1.getCoordCentroid()))
 
 if __name__ == "__main__":
     main()
